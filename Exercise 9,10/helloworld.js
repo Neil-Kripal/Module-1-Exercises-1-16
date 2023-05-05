03d212e71b062f1888c10957ef3aae2032b23f41
Neil-Kripal <132238703+Neil-Kripal@users.noreply.github.com>
2023-05-05 14:58:17 +1000

    Add files via upload

---
+++ Exercise 9,10/helloworld.js
@@ -0,0 +1,43 @@
+// Hello and Name function
+function helloWorld(name) {
+    console.log("Hello " + name + "!");
+  }
+  
+
+// Addtion Function
+// Replace x & y with desired numbers to be outputed
+function add(x, y) {
+    const result = x + y;
+    console.log(`${x} + ${y} = ${result}`);
+    return result;
+  }
+
+// Subtraction Function
+// Replace x & y with desired numbers to be outputed
+function subtract(x, y) {
+    const result = x - y;
+    console.log(`${x} - ${y} = ${result}`);
+    return result;
+  }
+
+// Subtraction Function
+// Replace x & y with desired numbers to be outputed
+  function multiply(x, y) {
+    const result = x * y;
+    console.log(`${x} * ${y} = ${result}`);
+    return result;
+  }
+
+// Divsion Function
+// Replace x & y with desired numbers to be outputed
+  function divide(x, y) {
+    const result = x / y;
+    console.log(`${x} / ${y} = ${result}`);
+    return result;
+  }
+  helloName("Mirza, here are the answers to your question");
+
+  add(96, 109);
+  subtract(215,87);
+  multiply(298, 37);
+  divide(986,45);
